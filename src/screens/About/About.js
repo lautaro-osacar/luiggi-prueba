c19f60e67ad0172c760ba42b500b9fc0e0a34aec
Lautaro Osácar <lautaro.osacar@onetree.com>
2021-07-01 10:52:25 -0300

    about section

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -1,7 +1,32 @@
-import Header from "../../components/Header/Header"
+import React from "react";
+import Header from "../../components/Header/Header";
+import "./styles.css";
+import contentJson from "./content.json";
 
 const About = () => {
-  return <Header />;
+  console.log(contentJson);
+  return (
+    <React.Fragment>
+      <Header />
+      <div id="about-hz">
+        <div id="about-left">
+          <span id="about-description">
+            {contentJson.description.split("\n").map((str) => (
+              <p>{str}</p>
+            ))}
+          </span>
+        </div>
+        <div id="about-right">
+          <img
+            id="about-logo"
+            src="./logo-cropped.png"
+            alt="logo"
+            //style={{ height: "200px" }}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default About;
