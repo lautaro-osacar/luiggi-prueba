be3e536ff80022d894b3a365698d282afeaf11b9
Lautaro Osácar <lautaro.osacar@onetree.com>
2021-07-06 12:40:35 -0300

    first mobile version

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import ArtistsList from "../../components/ArtistsList/ArtistsList";
 
 const Home = () => {
   const [selectedArtist, setSelectedArtist] = useState();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const listener = (evt) => setIsMobile(!evt.matches);
+    try {
+      const watcher = window.matchMedia("(min-width: 768px)");
+      listener(watcher);
+      watcher.addEventListener("change", listener);
+      return () => {
+        window
+          .matchMedia("(min-width: 768px)")
+          .removeEventListener("change", listener);
+      };
+    } catch (err) {
+      //deprecated but working on safari
+      const watcher = window.matchMedia("(min-width: 768px)");
+      listener(watcher);
+      watcher.addListener(listener);
+      return () => {
+        window.matchMedia("(min-width: 768px)").removeListener(listener);
+      };
+    }
+  }, []);
 
   return (
     <div className={selectedArtist && "with-background"}>
-      {!!selectedArtist && (
+      {!isMobile && !!selectedArtist && (
         <div
           id="background"
           style={{ backgroundImage: `url(${selectedArtist})` }}
@@ -18,13 +41,14 @@ const Home = () => {
         onHoverArtist={(artist) => setSelectedArtist(artist.photo)}
         onHoverArtistEnd={() => setSelectedArtist(null)}
       />
-      {!selectedArtist && (
+      {!isMobile && !selectedArtist && (
         <div>
           <div id="background-bottom" />
           <img id="background-bottom-logo" src="./crop2.gif" alt="logo" />
           <span id="background-bottom-year">@2021</span>
         </div>
       )}
+      {isMobile && <img id="mobile-bottom-logo" src="./crop2.gif" alt="logo" />}
     </div>
   );
 };
