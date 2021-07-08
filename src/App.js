2468e82dc305df2f479e241459dde9d2b54b7435
Lautaro Osácar <lautaro.osacar@onetree.com>
2021-07-08 12:28:24 -0300

    about mobile

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import About from "./screens/About/About";
 import Artist from "./screens/Artist/Artist";
+import React from "react";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
-  
   return (
     <div className={`App`}>
       <Router>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/artist/:id">
-            <Artist />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <React.Fragment>
+          <ScrollToTop />
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/artist/:id">
+              <Artist />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </React.Fragment>
       </Router>
     </div>
   );
