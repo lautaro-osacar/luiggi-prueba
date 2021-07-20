7de5d46e81e407eb6e979027c19368153c6bea40
Lautaro Osácar <lautaro.osacar@onetree.com>
2021-07-20 15:49:10 -0300

    preload background images

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect, useRef } from "react";
 import Header from "../../components/Header/Header";
 import ArtistsList from "../../components/ArtistsList/ArtistsList";
 import ScrollIndicator from "../../components/ScrollIndicator/ScrollIndicator";
+import artistsJson from "../../artists.json";
 
 const Home = () => {
   const [selectedArtist, setSelectedArtist] = useState();
   const [isMobile, setIsMobile] = useState(false);
   const appRef = useRef(null);
+  let loaded = false;
 
   useEffect(() => {
-    const listener = (evt) => setIsMobile(!evt.matches);
+    const listener = (evt) => {
+      setIsMobile(!evt.matches);
+      loaded = true;
+    };
     try {
       const watcher = window.matchMedia("(min-width: 768px)");
       listener(watcher);
@@ -29,6 +34,19 @@ const Home = () => {
       };
     }
   }, []);
+
+  const preloadImage = (imageSource) => {
+    let img = new Image();
+    img.src = imageSource;
+  };
+
+  useEffect(() => {
+    if (!isMobile && loaded) {
+      Object.values(artistsJson).forEach((artist) =>
+        preloadImage(artist.photoBackground)
+      );
+    }
+  }, [loaded, isMobile]);
 
   return (
     <div className={selectedArtist && "with-background"} ref={appRef}>
