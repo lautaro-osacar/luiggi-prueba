c60bdb35bea581501b20bcea5f8df820550d0975
Lautaro Osácar <lautaro.osacar@onetree.com>
2021-07-20 23:37:06 -0300

    font preload

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import About from "./screens/About/About";
 import Artist from "./screens/Artist/Artist";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import WebFont from "webfontloader";
 
 function App() {
+  const [isFontLoaded, setFontLoaded] = useState(false)
+
+  useEffect(() => {
+    WebFont.load({
+      custom: {
+        families: ["DrukMedium"],
+      },
+      fontactive: () => {
+        setFontLoaded(true)
+      }
+    });
+  }, []);
+
+  if (!isFontLoaded) {
+    return null
+  }
+
   return (
     <div className={`App`}>
       <Router>
